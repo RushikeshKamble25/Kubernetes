f5c7b0807cba2aa67e31839db6be7c319e56402d
RushikeshKamble25 <rushikeshkamble0025@gmail.com>
2026-03-29 18:26:25 +0530

    Update only-master.sh

--- only-master.sh
+++ only-master.sh
@@ -1,19 +1,9 @@
-sudo apt update -y
-sudo apt install docker.io -y
+sudo su
+hostnamectl set-hostname master
+exec bash
 
-sudo mkdir -p /etc/containerd
-sudo containerd config default | sudo tee /etc/containerd/config.toml
-sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+sudo kubeadm init (this will generate the join token command which needs to be executed on the worker node)
 
-sudo systemctl restart docker containerd
-sudo systemctl enable docker containerd
-
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt update -y
-sudo apt install kubeadm kubectl kubelet -y
-
-sudo apt --purge remove apparmor -y 
-sudo reboot
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
