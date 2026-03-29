d96a8dff84afeeed5196621a7da735eec4470b35
RushikeshKamble25 <rushikeshkamble0025@gmail.com>
2026-03-29 19:31:20 +0530

    Update only-slave.sh

--- only-slave.sh
+++ only-slave.sh
@@ -1,2 +1,13 @@
-1. Paste the join command you got from the master node and append `--v=5` at the end but first use sudo su command to become root (avoid using sudo your-token).
+## Execute on ALL of your Worker Node's
+
+
+
+1. Perform pre-flight checks
+
+
+sudo kubeadm reset pre-flight checks
+
+
+2. Paste the join command you got from the master node and append `--v=5` at the end but first use sudo su command to become root (avoid using sudo your-token).
    <your-token --v=5>
+   
